a01863237a904ad360f9b0b49ca532cab15d4048
Aisha Ikram <aisha.ikram@gmail.com>
2021-08-11 19:40:30 +0100

    Added Receive Message Functionaility and Refactored code accordingly

--- Program.cs
+++ Program.cs
@@ -9,38 +9,67 @@ namespace QueueApp
     class Program
     {
         const string  connectionstr = "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=storageacct11082021;AccountKey=7SMsPBZ29t/xqSR5fHXaPOi8ssscJEbpnEcbsxgXcnSMov6GUohuEDs6l6kkKwFzQPQ7r39IkgRHMsgXTbrZGA==";
-        static void Main(string[] args)
+        const string queueName = "queue11082021";
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Queue App started, it will create a queue in your storage account and send and receive the messages");
+            
             SendQueueMessageAsync("This is the content of the queue message... which will be 48K in Base 64").Wait();
-         /*   Console.WriteLine("Receiving Message from Queue....");
-           CloudQueueMessage msg = await queue.GetMessageAsync();
 
-            if (msg != null)
-            {
-                // Process the message
-                Console.WriteLine("Message Received is" + msg.ToString());
-                Console.WriteLine("Delete Message from Queue after Processing (otherwise msg will come back in 30 secs to manage at-least once delivery)....");
-                await queue.DeleteMessageAsync(message);
-            }*/
+            string value = await ReceiveQueueMessageAsync();
+            Console.WriteLine($"Received->: {value}");
+                  
              Console.WriteLine("Program Finished");
         }
 
-        static async Task SendQueueMessageAsync(string Message)
+        static CloudQueue GetQueue()
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionstr);
 
             CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
 
-            CloudQueue queue = queueClient.GetQueueReference("queue11082021");
+            return queueClient.GetQueueReference("queue11082021");
+        
+                    
+        }
+        static async Task SendQueueMessageAsync(string Message)
+        {
+            //get reference to the queue
+            CloudQueue queue = GetQueue();
             bool createdQueue = await queue.CreateIfNotExistsAsync();
             if (createdQueue)
             {
-                Console.WriteLine("The queue queue11082021 is created.");
+                Console.WriteLine($"The queue {queueName} is created.");
             }
+       
             Console.WriteLine("Sending Message to Queue....");
             CloudQueueMessage queMessage = new CloudQueueMessage(Message);
             await queue.AddMessageAsync(queMessage);
+        }
+
+        static async Task<string> ReceiveQueueMessageAsync()
+        {
+             //get reference to the queue
+            CloudQueue queue = GetQueue();
+            bool exists = await queue.ExistsAsync();
+            if (exists)
+            {
+                Console.WriteLine("Receiving Message from Queue....");
+                CloudQueueMessage msg = await queue.GetMessageAsync();
+
+                if (msg != null)
+                {
+                    //convert to string
+                    string messageContent = msg.AsString;
+                    // Process the message
+                    Console.WriteLine("Message Received is" + msg.AsString);
+                    Console.WriteLine("Delete Message from Queue after Processing (otherwise msg will come back in 30 secs to manage at-least once delivery)....");
+                    await queue.DeleteMessageAsync(msg);         
+                    return messageContent;
+                }
+                
+            }
+            return "Queue empty or not created";
         }
     }
 }
